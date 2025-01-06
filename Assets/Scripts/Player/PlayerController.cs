f9f68739b5e95ca4406946e747cdc6d00e453e18
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2025-01-05 16:54:31 -0800

    23. Player Dash setup with dash speed and dash cooldown

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -15,16 +15,29 @@ public class PlayerController : MonoBehaviour
     //
     public bool FacingLeft { get { return facingLeft; } set { facingLeft = value; } }
 
-    [SerializeField] private float moveSpeed = 1;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashCoolDownTime;
+    [SerializeField] private TrailRenderer myTrailRenderer;
 
     private PlayerControls playerControls;
     private Vector2 movement; // stores values coming in from user
     private Rigidbody2D rb;
     private Animator myAnimator;
     private SpriteRenderer mySpriteRenderer;
+    private float originalMoveSpeed;
+    private bool isDashing = false;
 
 
     private bool facingLeft = false;
+
+    private void Start()
+    {
+        // subscribe to the dash event
+        playerControls.Combat.Dash.performed += _ => Dash();
+        myTrailRenderer.emitting = false;
+    }
 
     private void Awake()
     {
@@ -34,6 +47,8 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
+
+        originalMoveSpeed = moveSpeed;
     }
 
     private void OnEnable()
@@ -72,9 +87,6 @@ public class PlayerController : MonoBehaviour
 
     void AdjustPlayerFacingDirection()
     {
-        //Vector3 mousePos = Mouse.current.position.ReadValue();
-        //Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
-        //mySpriteRenderer.flipX = mousePos.x < playerScreenPoint.x;
 
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
@@ -90,5 +102,27 @@ public class PlayerController : MonoBehaviour
             FacingLeft = false;
         }
 
+    }
+
+    private void Dash()
+    {
+        
+        if (!isDashing)
+        {
+            isDashing = true;
+            myTrailRenderer.emitting = true;
+            moveSpeed *= dashSpeed;
+            StartCoroutine(EndDashRoutine());
+        }
+        
+    }
+
+    private IEnumerator EndDashRoutine()
+    {
+        yield return new WaitForSeconds(dashTime);
+        moveSpeed = originalMoveSpeed;
+        myTrailRenderer.emitting = false;
+        yield return new WaitForSeconds(dashCoolDownTime);
+        isDashing = false;
     }
 }
